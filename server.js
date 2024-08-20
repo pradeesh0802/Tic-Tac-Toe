a36048cd2b4b8c8d610961aa5d3ca6bbec3ea604
pradeesh0802 <pradeeshpalanisamy@gmail.com>
2024-08-20 07:48:04 +0530

    exception handling

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const db = require('./src/db');
 
 const app = express();
@@ -14,12 +15,10 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, '..', 'form', 'build')));
+
 app.post('/api/register', (req, res) => {
   const { username, password, confirmPassword } = req.body;
-
-  if (password !== confirmPassword) {
-    return res.status(400).json({ error: 'Password and Confirm Password do not match' });
-  }
 
   const insertUserQuery = 'INSERT INTO users (username, password, confirmPassword, active) VALUES (?, ?, ?, "Y")';
   db.query(insertUserQuery, [username, password, confirmPassword], (err) => {
@@ -48,6 +47,28 @@ app.get('/api/check-username/:username', (req, res) => {
 
     res.status(200).json({ exists: false });
   });
+});
+
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  const loginQuery = 'SELECT * FROM users WHERE username = ? AND password = ? AND active = "Y"';
+  db.query(loginQuery, [username, password], (err, results) => {
+    if (err) {
+      console.error('Database query error:', err);
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (results.length > 0) {
+      res.status(200).json({ message: 'Login successful'});
+    } else {
+      res.status(401).json({ error: 'Invalid username or password' });
+    }
+  });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'form', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
